78604793a93211dd3d46b61fdc5105340e46c5d1
LeandroAlcantara-1997 <leandro1997silva97@gmail.com>
2021-08-23 19:20:00 -0300

    Project created

---
+++ src/main/java/com/github/LeandroAlcantara1997/Person_API_REST/PersonApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.github.LeandroAlcantara1997.Person_API_REST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/LeandroAlcantara1997/Person_API_REST/PersonApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.LeandroAlcantara1997.Person_API_REST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

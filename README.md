4f1939c09bf4f2d86e60ce9e2b109faf7b18f363
LeandroAlcantara-1997 <leandro1997silva97@gmail.com>
2021-08-28 18:24:42 -0300

    entity created

---
+++ README.md
@@ -0,0 +1,15 @@
+# Requiriments
+
+* Windows
+* Java 11
+* Maven 3.8
+* Intellij
+
+# Spring Boot
+
+* Spring Web
+* JPA
+* DevTools
+* Loombok
+* Actuator
+* H2 Database
---
+++ src/main/java/com/github/LeandroAlcantara1997/Person_API_REST/enums/PhoneType.java
@@ -0,0 +1,15 @@
+package com.github.LeandroAlcantara1997.Person_API_REST.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum PhoneType {
+
+    HOME("Home"),
+    MOBILE("Mobile"),
+    COMMERCIAL("Commercial");
+
+    private final String description;
+}

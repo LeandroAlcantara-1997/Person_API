8489033e41eb643fbea1a41595d4fbfc08320fb6
LeandroAlcantara-1997 <leandro1997silva97@gmail.com>
2021-08-29 17:56:49 -0300

    created README.md for documentation

--- src/main/java/com/github/LeandroAlcantara1997/Person_API_REST/service/PersonService.java
+++ src/main/java/com/github/LeandroAlcantara1997/Person_API_REST/service/PersonService.java
@@ -6,12 +6,15 @@ import com.github.LeandroAlcantara1997.Person_API_REST.entity.Person;
 import com.github.LeandroAlcantara1997.Person_API_REST.exception.PersonNotFoundException;
 import com.github.LeandroAlcantara1997.Person_API_REST.mapper.PersonMapper;
 import com.github.LeandroAlcantara1997.Person_API_REST.repository.PersonRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonService {
 
     private PersonRepository personRepository;
